42350cae3e78c1fc8e3464f8bab3b01fdc659508
Arush Handa <handaarush1237@gmail.com>
2026-01-11 13:41:39 -0500

    Add dictionary for defining all gesture classes, and global variables to control the process

--- collect_data.py
+++ collect_data.py
@@ -4,3 +4,15 @@ import os
 import time
 import numpy as np
 
+GESTURES = {
+    '1': "open_hand",
+    '2': "fist",
+    '3': "v_sign",
+    '4': "index_pointing",
+    '5': "no_gesture"
+}
+
+IMG_SIZE = 224
+CAPTURE_INTERVAL = 0.2
+DATASET_PATH = "dataset/raw"
+PADDING = 30 #Padding for bouding box aroung the hand in the image
