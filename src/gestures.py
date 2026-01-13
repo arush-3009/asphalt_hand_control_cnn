e17c482d7a53b094f4eaa7f2e7619e77da5b2d45
Arush Handa <handaarush1237@gmail.com>
2026-01-13 04:21:29 -0500

    Comment out everything excpet get_hand_position and get_steering_direction and __init__ function. Add initialization of GesturePredictor class to init

--- src/gestures.py
+++ src/gestures.py
@@ -1,6 +1,9 @@
 import cv2
 import math
 
+from ml.inference import GesturePredictor
+import ml.config as cnn_config
+
 
 class Gestures:
 
@@ -15,75 +18,14 @@ class Gestures:
         }
         self.config = config
 
-    def calc_distance(self, p1, p2):
-        return math.sqrt(((p1.x - p2.x)**2) + ((p1.y - p2.y)**2))
-    
-    def get_hand_size(self, landmarks):
-        wrist = landmarks[0]
-        middle_base = landmarks[9]
-        return self.calc_distance(wrist, middle_base)
+        #Initialize a GesturePredictor object in order to predict hand gesture using the CNN
+        self.predictor = GesturePredictor(path_to_trained_model= (cnn_config.MODELS_DIR / cnn_config.MODEL_SAVE_NAME),  mean_norm=cnn_config.NORMALIZE_MEAN, std_norm=cnn_config.NORMALIZE_STD, device=cnn_config.DEVICE, img_size=cnn_config.IMG_SIZE)
 
     def get_hand_position(self, landmarks):
         wrist = landmarks[0]
         self.gestures_data['hand_x'] = wrist.x
         return wrist.x
 
-    def is_fist(self, landmarks, display_output=False, img=None):
-        if len(landmarks) == 0: return None
-        wrist = landmarks[0]
-        middle_finger_base = landmarks[9]
-        hand_size = self.get_hand_size(landmarks)
-        ret = True
-        tips_and_thresholds = {8: 1, 12: 1, 16: 0.85, 20: 0.85}
-        for fingertip in tips_and_thresholds:
-            dist = self.calc_distance(landmarks[fingertip], wrist)
-            ratio = dist/hand_size
-            if ratio > tips_and_thresholds[fingertip]:
-                ret = False
-                break
-        
-        thumb_tip = landmarks[4]
-        ring_knuckle = landmarks[14]
-        thumb_to_ring = self.calc_distance(thumb_tip, ring_knuckle)
-        thumb_ring_hand_size_ratio = thumb_to_ring/hand_size
-        if thumb_ring_hand_size_ratio > 0.35: ret = False
-
-        if display_output:
-            if img is None:
-                raise ValueError("img required when display_output=True")
-            cv2.putText(img, f'Fist: {ret}', (50, 300), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
-
-        self.gestures_data['fist'] = ret
-        return ret
-    
-    def is_open(self, landmarks, display_output=False, img=None):
-        if len(landmarks) == 0: return None
-        wrist = landmarks[0]
-        middle_finger_base = landmarks[9]
-        hand_size = self.get_hand_size(landmarks)
-        ret = True
-        tips_and_thresholds = {8: 1.6, 12: 1.8, 16: 1.6, 20: 1.4}
-        for fingertip in tips_and_thresholds:
-            dist = self.calc_distance(landmarks[fingertip], wrist)
-            ratio = dist/hand_size
-            if ratio < tips_and_thresholds[fingertip]:
-                ret = False
-                break
-        
-        thumb_tip = landmarks[4]
-        ring_knuckle = landmarks[14]
-        thumb_to_ring = self.calc_distance(thumb_tip, ring_knuckle)
-        thumb_ring_hand_size_ratio = thumb_to_ring/hand_size
-        if thumb_ring_hand_size_ratio < 0.35: ret = False
-
-        if display_output:
-            if img is None:
-                raise ValueError("img required when display_output=True")
-            cv2.putText(img, f'Open Hand: {ret}', (50, 250), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
-
-        self.gestures_data['open_hand'] = ret
-        return ret
-    
     def get_steering_direction(self, landmarks, display_output=False, img=None):
         if len(landmarks) == 0: return 'center'
         x = landmarks[0].x
@@ -109,60 +51,125 @@ class Gestures:
         self.gestures_data['steering_direction'] = direction
 
         return direction
+
+    # def calc_distance(self, p1, p2):
+    #     return math.sqrt(((p1.x - p2.x)**2) + ((p1.y - p2.y)**2))
     
-    def is_index_pointing(self, landmarks, display_output=False, img=None):
+    # def get_hand_size(self, landmarks):
+    #     wrist = landmarks[0]
+    #     middle_base = landmarks[9]
+    #     return self.calc_distance(wrist, middle_base)
 
-        if len(landmarks) == 0: return None
-        hand_size = self.get_hand_size(landmarks)
-        wrist = landmarks[0]
-        tips_and_thresholds = {8: 1.65, 12: 0.8, 16: 0.8, 20: 0.8}
-        ret = True
-        for fingertip in tips_and_thresholds:
-            dist = self.calc_distance(wrist, landmarks[fingertip])
-            ratio = dist/hand_size
-            if fingertip == 8 and ratio < tips_and_thresholds[fingertip]:
-                ret = False
-                break
-            elif fingertip != 8 and ratio > tips_and_thresholds[fingertip]:
-                ret = False
-                break
-            
-        if display_output:
-            if img is None:
-                raise ValueError("img required when display_output=True")
-            cv2.putText(img, f'Index Pointing: {ret}', (50, 400), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
+    # def is_fist(self, landmarks, display_output=False, img=None):
+    #     if len(landmarks) == 0: return None
+    #     wrist = landmarks[0]
+    #     middle_finger_base = landmarks[9]
+    #     hand_size = self.get_hand_size(landmarks)
+    #     ret = True
+    #     tips_and_thresholds = {8: 1, 12: 1, 16: 0.85, 20: 0.85}
+    #     for fingertip in tips_and_thresholds:
+    #         dist = self.calc_distance(landmarks[fingertip], wrist)
+    #         ratio = dist/hand_size
+    #         if ratio > tips_and_thresholds[fingertip]:
+    #             ret = False
+    #             break
         
-        self.gestures_data['index'] = ret
-        
-        return ret
+    #     thumb_tip = landmarks[4]
+    #     ring_knuckle = landmarks[14]
+    #     thumb_to_ring = self.calc_distance(thumb_tip, ring_knuckle)
+    #     thumb_ring_hand_size_ratio = thumb_to_ring/hand_size
+    #     if thumb_ring_hand_size_ratio > 0.35: ret = False
+
+    #     if display_output:
+    #         if img is None:
+    #             raise ValueError("img required when display_output=True")
+    #         cv2.putText(img, f'Fist: {ret}', (50, 300), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
+
+    #     self.gestures_data['fist'] = ret
+    #     return ret
     
-    def is_v(self, landmarks, display_output=False, img=None):
-
-        if len(landmarks) == 0: return None
-
-        hand_size = self.get_hand_size(landmarks)
-        wrist = landmarks[0]
-        tips_and_thresholds = {8: 1.7, 12: 1.7, 16: 1, 20: 1}
-        ret = True
-
-        extended_fingers = {8, 12}
-
-        for fingertip in tips_and_thresholds:
-            dist = self.calc_distance(wrist, landmarks[fingertip])
-            ratio = dist / hand_size
-
-            if fingertip in extended_fingers and ratio < tips_and_thresholds[fingertip]:
-                ret = False
-                break
-            elif fingertip not in extended_fingers and ratio > tips_and_thresholds[fingertip]:
-                ret = False
-                break
-
-        if display_output:
-            if img is None:
-                raise ValueError("img required when display_output=True")
-            cv2.putText(img, f'V showing: {ret}', (50, 350), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
+    # def is_open(self, landmarks, display_output=False, img=None):
+    #     if len(landmarks) == 0: return None
+    #     wrist = landmarks[0]
+    #     middle_finger_base = landmarks[9]
+    #     hand_size = self.get_hand_size(landmarks)
+    #     ret = True
+    #     tips_and_thresholds = {8: 1.6, 12: 1.8, 16: 1.6, 20: 1.4}
+    #     for fingertip in tips_and_thresholds:
+    #         dist = self.calc_distance(landmarks[fingertip], wrist)
+    #         ratio = dist/hand_size
+    #         if ratio < tips_and_thresholds[fingertip]:
+    #             ret = False
+    #             break
         
-        self.gestures_data['v'] = ret
+    #     thumb_tip = landmarks[4]
+    #     ring_knuckle = landmarks[14]
+    #     thumb_to_ring = self.calc_distance(thumb_tip, ring_knuckle)
+    #     thumb_ring_hand_size_ratio = thumb_to_ring/hand_size
+    #     if thumb_ring_hand_size_ratio < 0.35: ret = False
+
+    #     if display_output:
+    #         if img is None:
+    #             raise ValueError("img required when display_output=True")
+    #         cv2.putText(img, f'Open Hand: {ret}', (50, 250), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
+
+    #     self.gestures_data['open_hand'] = ret
+    #     return ret
+
+    
+    # def is_index_pointing(self, landmarks, display_output=False, img=None):
+
+    #     if len(landmarks) == 0: return None
+    #     hand_size = self.get_hand_size(landmarks)
+    #     wrist = landmarks[0]
+    #     tips_and_thresholds = {8: 1.65, 12: 0.8, 16: 0.8, 20: 0.8}
+    #     ret = True
+    #     for fingertip in tips_and_thresholds:
+    #         dist = self.calc_distance(wrist, landmarks[fingertip])
+    #         ratio = dist/hand_size
+    #         if fingertip == 8 and ratio < tips_and_thresholds[fingertip]:
+    #             ret = False
+    #             break
+    #         elif fingertip != 8 and ratio > tips_and_thresholds[fingertip]:
+    #             ret = False
+    #             break
             
-        return ret
+    #     if display_output:
+    #         if img is None:
+    #             raise ValueError("img required when display_output=True")
+    #         cv2.putText(img, f'Index Pointing: {ret}', (50, 400), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
+        
+    #     self.gestures_data['index'] = ret
+        
+    #     return ret
+    
+    # def is_v(self, landmarks, display_output=False, img=None):
+
+    #     if len(landmarks) == 0: return None
+
+    #     hand_size = self.get_hand_size(landmarks)
+    #     wrist = landmarks[0]
+    #     tips_and_thresholds = {8: 1.7, 12: 1.7, 16: 1, 20: 1}
+    #     ret = True
+
+    #     extended_fingers = {8, 12}
+
+    #     for fingertip in tips_and_thresholds:
+    #         dist = self.calc_distance(wrist, landmarks[fingertip])
+    #         ratio = dist / hand_size
+
+    #         if fingertip in extended_fingers and ratio < tips_and_thresholds[fingertip]:
+    #             ret = False
+    #             break
+    #         elif fingertip not in extended_fingers and ratio > tips_and_thresholds[fingertip]:
+    #             ret = False
+    #             break
+
+    #     if display_output:
+    #         if img is None:
+    #             raise ValueError("img required when display_output=True")
+    #         cv2.putText(img, f'V showing: {ret}', (50, 350), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
+        
+    #     self.gestures_data['v'] = ret
+            
+    #     return ret
