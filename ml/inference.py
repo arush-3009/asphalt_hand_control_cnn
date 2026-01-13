ead6638219955627d57f0a6c2499844a9ae9c5c3
Arush Handa <handaarush1237@gmail.com>
2026-01-13 03:03:16 -0500

    Add the preprocess image method

--- ml/inference.py
+++ ml/inference.py
@@ -1,4 +1,7 @@
 import torch
+from torchvision import transforms
+import cv2
+from PIL import Image
 
 from ml.model import GestureCNN
 import ml.config as config
@@ -11,7 +14,7 @@ class GesturePredictor():
         self.device = device
         self.model.load_state_dict(torch.load(path_to_trained_model, map_location=self.device))
     
-    def preprocess_image(self, frame, bbox):
+    def preprocess_image(self, frame, bbox, mean_norm, std_norm, img_size):
         """
         Get an OpenCV numpy frame, cropout out region of the hand, resize it and return as a Normalized Tensor
         """
@@ -19,4 +22,18 @@ class GesturePredictor():
         #Here, the input parameter frame will be an OpenCV (i.e. BGR) Numpy array of pixels. 
         #This needs to be processed and returned in the tensor format the model expects.
 
+        x_min, y_min, x_max, y_max = bbox
+        frame_cropped = frame[x_min:x_max, y_min:y_max]
+        frame_cropped = cv2.cvtColor(frame_cropped, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_cropped, (img_size, img_size))
+
+        pil_image = Image.fromarray(frame_resized)
+
+        transformation = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean_norm, std=std_norm)
+        ])
+
+        tensor = transformation(pil_image)
+        return tensor
         
